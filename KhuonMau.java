52c6d48d2776f4ad2d0724facaa848f132bb0d14
thuynt8894 <46840844+thuynt8894@users.noreply.github.com>
2019-02-19 18:26:50 +0700

    Add files via upload

---
+++ KhuonMau.java
@@ -0,0 +1,12 @@
+package Assignment6;
+
+public class KhuonMau {
+        public String name;
+        public String phone;
+
+        public KhuonMau(String name, String phone) {
+            this.name = name;
+            this.phone = phone;
+
+        }
+}
---
+++ PhoneBook.java
@@ -0,0 +1,72 @@
+package Assignment6;
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class PhoneBook extends Phone {
+    ArrayList<KhuonMau> listPhone;
+
+    public PhoneBook(ArrayList<KhuonMau> listPhone) {
+        this.listPhone = listPhone;
+    }
+
+    public void insertPhone(String name, String phone) {
+        if(listPhone.isEmpty()){
+            listPhone.add(new KhuonMau(name,phone));
+        }
+        else {
+            for(int i=0; i<listPhone.size();i++){
+                if(listPhone.get(i).name.equals(name)){
+                    if(listPhone.get(i).phone.equals(phone)){
+                        System.out.println("Thông tin khách hàng đã có trong danh bạ");
+                    }
+                    else {
+                        listPhone.add(new KhuonMau(name,phone));
+                    }
+                    break;
+                }
+                else {
+                    listPhone.add(new KhuonMau(name,phone));
+                }
+            }
+        }
+    }
+
+    @Override
+    void removePhone(String name) {
+        for(int i = 0 ; i<listPhone.size();i++){
+            if(name.equals(listPhone.get(i).name)){
+                listPhone.remove(listPhone.get(i));
+                System.out.println("Đã xóa số điện thoại"+name);
+            }
+        }
+    }
+
+    @Override
+    void updatePhone(String name, String newphone) {
+        for (int i = 0; i < listPhone.size(); i++) {
+            if(name.equals(listPhone.get(i).name)){
+                listPhone.get(i).phone=newphone;
+                System.out.println("Đã thay đổi số điện thoại");
+            }
+        }
+    }
+
+    @Override
+    void searchPhone(String name) {
+        for (int i = 0; i < listPhone.size(); i++) {
+            if(name.equals(listPhone.get(i).name)){
+                System.out.println(listPhone.get(i).phone);
+            }
+        }
+    }
+
+    @Override
+    void sort() {
+    }
+
+    public void getData() {
+        for (int i = 0; i < listPhone.size(); i++) {
+            System.out.println(listPhone.get(i).name);
+        }
+    }
+}
